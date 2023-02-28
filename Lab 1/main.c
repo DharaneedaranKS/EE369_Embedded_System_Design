5e595f7dbce58cbcdfcee8d40bd6001cf6f4cb3a
Arif Kalluru <97300325+Arif-Kalluru@users.noreply.github.com>
2023-02-28 13:03:25 +0530

    Update main.c

--- Lab 1/main.c
+++ Lab 1/main.c
@@ -11,50 +11,50 @@
 
 int main(void)
 {
-  WDOG_Handle myWDog;
-  myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
-  WDOG_disable(myWDog);
+    WDOG_Handle myWDog;
+    myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
+    WDOG_disable(myWDog);
 
-  CLK_Handle myClk;
-  PLL_Handle myPll;
-  myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
-  myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
+    CLK_Handle myClk;
+    PLL_Handle myPll;
+    myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
+    myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
 
-  // Using internal clock 1
-  // For using internal clock 2: CLK_Osc2Src_Internal
-  CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
+    // Using internal clock 1
+    // For using internal clock 2: CLK_Osc2Src_Internal
+    CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
 
-  // Base clock is 10Mhz, so PLL will do 10 * 12 / 2 = 60Mhz, which is max for this device
-  PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
+    // Base clock is 10Mhz, so PLL will do 10 * 12 / 2 = 60Mhz, which is max for this device
+    PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
 
-  GPIO_Handle myGpio;
-  myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
+    GPIO_Handle myGpio;
+    myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
 
-  // Set GPIO 0, 1, 2, 3 to output
-  GPIO_setMode(myGpio, GPIO_Number_0, GPIO_0_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_0, GPIO_Direction_Output);
-  GPIO_setMode(myGpio, GPIO_Number_1, GPIO_1_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_1, GPIO_Direction_Output);
-  GPIO_setMode(myGpio, GPIO_Number_2, GPIO_2_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_2, GPIO_Direction_Output);
-  GPIO_setMode(myGpio, GPIO_Number_3, GPIO_3_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_3, GPIO_Direction_Output);
+    // Set GPIO 0, 1, 2, 3 to output
+    GPIO_setMode(myGpio, GPIO_Number_0, GPIO_0_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_0, GPIO_Direction_Output);
+    GPIO_setMode(myGpio, GPIO_Number_1, GPIO_1_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_1, GPIO_Direction_Output);
+    GPIO_setMode(myGpio, GPIO_Number_2, GPIO_2_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_2, GPIO_Direction_Output);
+    GPIO_setMode(myGpio, GPIO_Number_3, GPIO_3_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_3, GPIO_Direction_Output);
 
-  // Set all of those to high, which means to NOT glow
-  GPIO_setHigh(myGpio, GPIO_Number_0);
-  GPIO_setHigh(myGpio, GPIO_Number_1);
-  GPIO_setHigh(myGpio, GPIO_Number_2);
-  GPIO_setHigh(myGpio, GPIO_Number_3);
-
-  while(1) {
-    // set low is glow
-    GPIO_setLow(myGpio, GPIO_Number_0);
-    // Delay in microseconds
-    DELAY_US(1000000);
-    // set high is off
+    // Set all of those to high, which means to NOT glow
     GPIO_setHigh(myGpio, GPIO_Number_0);
-    DELAY_US(1000000);
-  }
+    GPIO_setHigh(myGpio, GPIO_Number_1);
+    GPIO_setHigh(myGpio, GPIO_Number_2);
+    GPIO_setHigh(myGpio, GPIO_Number_3);
 
-  return 0;
+    while (1) {
+        // set low is glow
+        GPIO_setLow(myGpio, GPIO_Number_0);
+        // Delay in microseconds
+        DELAY_US(1000000);
+        // set high is off
+        GPIO_setHigh(myGpio, GPIO_Number_0);
+        DELAY_US(1000000);
+    }
+
+    return 0;
 }

f9fade6d18252205643b2fe0faa3381b8fa6b2e5
Arif Kalluru <97300325+Arif-Kalluru@users.noreply.github.com>
2023-02-28 12:57:28 +0530

    Update main.c

--- Lab 2/main.c
+++ Lab 2/main.c
@@ -5,51 +5,48 @@
 #include "f2802x_common/include/pll.h"
 #include "f2802x_common/include/wdog.h"
 
-
 #ifdef _FLASH
-    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart, (size_t)&RamfuncsLoadSize);
+	memcpy(&RamfuncsRunStart, &RamfuncsLoadStart, (size_t)&RamfuncsLoadSize);
 #endif
 
+int main(void)
+{
+    WDOG_Handle myWDog;
+    myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
+    WDOG_disable(myWDog);
 
-int main(void) {
-  WDOG_Handle myWDog;
-  myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
-  WDOG_disable(myWDog);
+    CLK_Handle myClk;
+    PLL_Handle myPll;
+    myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
+    myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
 
-  CLK_Handle myClk;
-  PLL_Handle myPll;
-  myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
-  myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
+    CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
+    PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
 
-  CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
+    GPIO_Handle myGpio;
+    myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
 
-  PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
+    GPIO_setMode(myGpio, GPIO_Number_0, GPIO_0_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_0, GPIO_Direction_Input);
+    GPIO_setMode(myGpio, GPIO_Number_1, GPIO_1_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_1, GPIO_Direction_Input);
+    GPIO_setMode(myGpio, GPIO_Number_2, GPIO_2_Mode_GeneralPurpose);
+    GPIO_setDirection(myGpio, GPIO_Number_2, GPIO_Direction_Output);
 
-  GPIO_Handle myGpio;
-  myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
+    uint16_t x;
+    uint16_t y;
 
-  GPIO_setMode(myGpio, GPIO_Number_0, GPIO_0_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_0, GPIO_Direction_Input);
-  GPIO_setMode(myGpio, GPIO_Number_1, GPIO_1_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_1, GPIO_Direction_Input);
-  GPIO_setMode(myGpio, GPIO_Number_2, GPIO_2_Mode_GeneralPurpose);
-  GPIO_setDirection(myGpio, GPIO_Number_2, GPIO_Direction_Output);
+    while (1) {
+        x = GPIO_getData(myGpio, GPIO_Number_0);
+        y = GPIO_getData(myGpio, GPIO_Number_1);
 
-  uint16_t x;
-  uint16_t y;
+        if (x && y)
+            GPIO_setLow(myGpio, GPIO_Number_2);
+        else
+            GPIO_setHigh(myGpio, GPIO_Number_2);
+        
+        DELAY_US(1000000);
+    }
 
-  while (1) {
-		x = GPIO_getData(myGpio, GPIO_Number_0);
-		y = GPIO_getData(myGpio, GPIO_Number_1);
-
-		if (x && y) {
-				GPIO_setLow(myGpio, GPIO_Number_2);
-		} else {
-				GPIO_setHigh(myGpio, GPIO_Number_2);
-		}
-
-		DELAY_US(1000000);
-	}
-
-  return 0;
+    return 0;
 }
